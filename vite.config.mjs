1ea46a4cc7ad277d1f10abf64afb46acf37fe922
JASON A TELLO <jasontello2003@gmail.com>
2026-06-22 11:19:55 -0700

    Document and publish Funfetti redesign (#1)

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,5 +1,17 @@
+import { resolve } from 'node:path';
+import { fileURLToPath } from 'node:url';
 import { defineConfig } from 'vite';
+
+const rootDirectory = fileURLToPath(new URL('.', import.meta.url));
 
 export default defineConfig({
   base: './',
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(rootDirectory, 'index.html'),
+        estimator: resolve(rootDirectory, 'estimator.html'),
+      },
+    },
+  },
 });
